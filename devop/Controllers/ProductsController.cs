54a50e4643d9f3a5b7999b9d1076d7a73be308f6
Meera Ram <rsmpriyan@gmail.com>
2026-01-08 16:56:33 +0400

    Add .NET project

---
+++ devop/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace devop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            ViewBag.ProductId = id;
+            return View();
+        }
+    }
+}
